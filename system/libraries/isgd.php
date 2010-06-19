6d3c57ea890c3924e1e77ff2e566b41b9cc42808
Thiago Avelino <avelinoo@me.com>
2010-06-19 14:09:58 -0300

    New LIB is.gd

---
+++ system/libraries/isgd.php
@@ -0,0 +1,54 @@
+<?php 
+class isgd { 
+	
+	public $url_long, $url_short, $error, $error_msg = NULL;
+	private $curl;
+	
+	public function __construct()
+	{
+		$this->curl = curl_init();
+		curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($this->curl, CURLOPT_FOLLOWLOCATION, 1);
+	}
+	
+	public function encode($url)
+	{
+		$urlCode = rawurlencode($url);
+		$urlApi = 'http://is.gd/api.php?longurl='.$urlCode;
+		curl_setopt($this->curl, CURLOPT_URL, $urlApi);
+		$return = curl_exec($this->curl);
+		$data = curl_getinfo($this->curl);
+		
+		if ($data['http_code'] == 200 && strlen($return) < 20)
+		{
+			$this->error = false;
+			$this->error_msg = NULL;
+			$this->url_short = $return;
+		}else
+		{
+			$this->error = true;
+			$this->error_msg = $return;
+			$this->url_short = NULL;
+		}
+		
+		$this->url_long = $url;
+		
+		return $this->error;
+	}
+	
+	public function decode($url)
+	{
+		if (substr($url, 0, 13) == 'http://is.gd/' || substr($url, 0, 6) == 'is.gd/')
+		{
+			curl_setopt($this->curl, CURLOPT_URL, $url);
+			curl_exec($this->curl);
+			$data = curl_getinfo($this->curl);
+			if ($data['http_code'] == 200 && $url != $data['url'])
+			{
+				return $data['url'];
+			}
+		}
+		return false;
+	}
+}
+?>
