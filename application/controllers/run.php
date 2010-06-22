da09481bfa4dc09701f80bf4e03ba3de3b640c7f
Thiago Avelino <avelinoo@me.com>
2010-06-22 00:01:17 -0300

    Zend Framework native and parser bugs fixed

--- application/controllers/run.php
+++ application/controllers/run.php
@@ -5,19 +5,6 @@ class RunController extends AppController {
 	}
 	function index()
 	{
-		iG_Zend::load("Zend.Service.Twitter");
-		
-		$twitter = new Zend_Service_Twitter('avelino0','4v3l1n005');
-		$response = $twitter->status->friendsTimeline();
-		
-		foreach($response AS $key=>$value)
-		{
-			echo "<p>";
-			echo "<b>".$value->user->name."</b> (".$value->user->screen_name.")<br />";
-			echo "<b>Msg:</b> ".$value->text;
-			echo "</p>";
-		}
-		
 		$this->layout = 'igrape';
 	}
 	function __destruct()
