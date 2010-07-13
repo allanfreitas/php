e6fa88d18ee8e6c76f18b4c39f7f6fba1ce05927
Thiago Avelino <thiagoavelinoster@gmail.com>
2010-07-13 00:34:07 -0300

    update

--- application/controllers/app.php
+++ application/controllers/app.php
@@ -1,24 +1,11 @@
 <?php
 define('INDEX', $conf['index_page']);
 class AppController extends Controller {
-	
-	public $_db = array();
-	public $_ORM = array();
-	public $_conf = array();
-	
-	function __construct()
-	{
-	}
-	
 	function before()
 	{
 	}
 	
 	function after()
-	{
-	}
-	
-	function __destruct()
 	{
 	}
 }
